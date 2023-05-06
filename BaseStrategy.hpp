c52cd55cf2424a3966efef1260ea48d4816f352c
Prakhar834 <prakhar.bitsup@gmail.com>
2023-05-06 22:05:21 +0530

    Removing unused variables

--- BaseStrategy.hpp
+++ BaseStrategy.hpp
@@ -9,10 +9,6 @@ class BaseStrategy {
     protected:
         std::vector<double> position;
         std::vector<std::string> instruments;
-
-        std::string positions_path;
-        std::string simulation_name;
-        std::string algo_name;
     public:
         BaseStrategy();
         BaseStrategy(std::unordered_map<std::string, std::string> params);
